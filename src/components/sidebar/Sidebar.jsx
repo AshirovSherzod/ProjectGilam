fe85a3167c97fe0119f45788950079d8d4d0b236
azamovcoder <azamovcoder@gmail.com>
2024-09-24 13:54:40 +0300

    uu

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,15 +1,18 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
+import React from "react";
 
 const Sidebar = () => {
   return (
     <section className="w-[250px] h-[100vh] fixed bg-white border-r-[1px] border-[#E0E0E0]">
-        <div className="h-[70px] border-b-[1px] flex items-center px-[20px] border-[#E0E0E0]">
-            <h1 className="font-bold text-[24px]">Your Logo</h1>
-            <h2>Salom</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum vitae animi dolorem!</p>
-        </div>
-        <NavLink></NavLink>
+      <div className="h-[70px] border-b-[1px] flex items-center px-[20px] border-[#E0E0E0]">
+        <h1 className="font-bold text-[24px]">Your Logo</h1>
+        <h2>Salom</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur
+          <span>adipisicing elit. Harum vitae animi dolorem!</span>
+        </p>
+      </div>
+      <NavLink></NavLink>
     </section>
   );
 };
