e02e14501e5661720b849e56541c9599d07c7e1b
AshirovSherzod <ashirovsherzod614@gmail.com>
2024-09-24 13:49:23 +0500

    task done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,6 +7,7 @@ const Sidebar = () => {
         <div className="h-[70px] border-b-[1px] flex items-center px-[20px] border-[#E0E0E0]">
             <h1 className="font-bold text-[24px]">Your Logo</h1>
             <h2>Salom</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum vitae animi dolorem!</p>
         </div>
         <NavLink></NavLink>
     </section>
