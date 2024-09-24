379497459c72168b891d8317d76e6b0247688d12
AshirovSherzod <ashirovsherzod614@gmail.com>
2024-09-24 13:33:17 +0500

    add

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,6 +6,7 @@ const Sidebar = () => {
     <section className="w-[250px] h-[100vh] fixed bg-white border-r-[1px] border-[#E0E0E0]">
         <div className="h-[70px] border-b-[1px] flex items-center px-[20px] border-[#E0E0E0]">
             <h1 className="font-bold text-[24px]">Your Logo</h1>
+            <h2>Salom</h2>
         </div>
         <NavLink></NavLink>
     </section>
